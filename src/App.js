3cd95c69089ab7847ed6ded1e9fac6f1659bece8
Connor Wong <connor@Connors-MacBook-Pro.local>
2020-04-01 22:43:39 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='App'>
+      {console.log('hi')/* content goes here */}
+      hi
+    </main>
+  );
+}
+
+export default App;
