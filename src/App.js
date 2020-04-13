c0bbb992d788b4e6319b556ac25561e7e1a54e8f
connorhw <connorwong93@gmail.com>
2020-04-12 20:53:56 -0700

    dummy data is printed to console for static client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+//import inventory from './inventory'
 
-function App() {
-  return (
-    <main className='App'>
-      {console.log('hi')/* content goes here */}
-      hi
-    </main>
-  );
+import PLANTS from './inventory'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    //replace whatever is in here with the future fetch.
+    const allPlants = PLANTS;
+    console.log(allPlants);
+  }
+
+  render() {
+    return (
+      <main className='App'>
+        {console.log('App component rendered successfully!')/* content goes here */}
+        {/*console.log(inventory)*/}
+        Plant Parenthood :)
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/inventory.js
@@ -0,0 +1,41 @@
+export default {
+    "plant1": [
+        {
+        "plant_name": "name1",
+        "water_per_day": 0,
+        "water_per_week": 2,
+        "environment": "indoors, sunny, room temp",
+        "fertilizer": "very very special type of ferilizer, test, test, test",
+        "repot": "every 6 months",
+        "maintenance_level": "low",
+        "fun_fact": "came from blah blah blah"
+        }
+    ],
+
+    "plant2": [
+        {
+            "plant_name": "name2",
+            "water_per_day": 1,
+            "water_per_week": 7,
+            "environment": "indoors, sunny, room temp",
+            "fertilizer": "very very special type of ferilizer, test2, test2, test2",
+            "repot": "every 1 months",
+            "maintenance_level": "high",
+            "fun_fact": "came from bleh bleh bleh"
+        }
+    ],
+    
+    "plant3": [
+        {
+            "plant_name": "name3",
+            "water_per_day": 0,
+            "water_per_week": 1,
+            "environment": "outdoors, in shade, 60-90 degrees",
+            "fertilizer": "very very special type of ferilizer, test3, test3, test3",
+            "repot": "every 8 months",
+            "maintenance_level": "very low",
+            "fun_fact": "came from bloh bloh bloh"
+        }
+    ]
+
+}
